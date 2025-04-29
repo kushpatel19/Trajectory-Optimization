9820f6b5508b04f21ceee1f47ad3b13c69029507
Jack Wischmeyer <jmwisch@umich.edu>
2025-04-28 23:01:22 -0400

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
 # ===== install.sh =====
-
 # Make sure pip is installed
 pip install --upgrade pip
-
 # Install required Python packages
-pip install pybullet
-pip install numpy
-pip install torch
-pip install matplotlib
+# pip install pybullet
+# pip install numpy
+# pip install torch
+# pip install matplotlib
 pip install tqdm
-pip install pybullet
+# pip install pybullet
 pip install imageio
 pip install opencv-python
 pip install numpngw
 pip install control
-
-
