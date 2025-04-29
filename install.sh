90284984ce989e364f07b2bc1847edda04d1cccb
kush <kush2002patelkp@gmail.com>
2025-04-28 23:25:55 -0400

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,14 @@
 # ===== install.sh =====
 # Make sure pip is installed
 pip install --upgrade pip
-# Install required Python packages
+
 # pip install pybullet
-<<<<<<< HEAD
 pip install numpy
 # pip install torch
 pip install matplotlib
-=======
 # pip install numpy
 # pip install torch
 # pip install matplotlib
->>>>>>> 355b052076ff607e5d5866ccc910b2764c0375d1
 pip install tqdm
 # pip install pybullet
 pip install imageio
